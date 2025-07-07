9211653b1e794f80f6f474c0891dc05c449031e2
Windalfin <windalfin@treeclouds.com>
2025-07-07 13:11:03 +0700

    feat: fix typo in string

--- recording/recording.go
+++ recording/recording.go
@@ -228,7 +228,7 @@ func captureRTSPStreamForCamera(ctx context.Context, cfg *config.Config, camera 
 				"-hls_flags", "independent_segments+delete_segments", // Ensure independent segments and clean up old ones
 				"-hls_segment_type", "mpegts", // Explicitly use MPEG-TS
 				"-reset_timestamps", "1", // Reset timestamps for each segment
-				"-hls_segment_filename", filepath.Join(cameraHLSDir, "segment_%Y%m%d_%H%M%S.ts"),
+				"-hls_segment_filename", filepath.Join(cameraHLSDir, "segment_%%Y%%m%%d_%%H%%M%%S.ts"),
 				hlsPlaylistPath,
 			)
 
